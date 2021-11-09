3d4e90d33f2b780e3972566afe52a53748b2f413
jeffpal <jeffpal.official@gmail.com>
2021-11-09 17:46:02 -0300

    create type Coordinate

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,7 @@
-export type ReadLineCallback= (line:string, writeStream?: null | NodeJS.WriteStream) => void;
+export type ReadLineCallback= (
+    line          : string,
+    remainingData?: boolean,
+    writeStream  ?: null | NodeJS.WriteStream
+) => void;
+
+export type Coordinate = [number, number];
